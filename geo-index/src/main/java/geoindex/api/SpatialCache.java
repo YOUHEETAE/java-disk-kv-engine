b5aebce6fe2a829f36af35069d416a93fde9776d
YOUHEETAE <dbgmlxo01@g.shingu.ac.kr>
2026-03-16 16:13:39 +0900

    refactor: GeoHash 미사용 메서드 및 관련 테스트 제거

--- geo-index/src/main/java/geoindex/api/SpatialCache.java
+++
@@ -1,9 +0,0 @@
-package geoindex.api;
-
-import java.util.List;
-
-public interface SpatialCache<T> {
-    List<PageResult<T>> search(double lat, double lng, double radiusKm);
-
-    void put(List<T> data);
-}
